23a90988d6e84b545eae4a1b2f3afdc792da8cc8
Jack <jackfan108@comtopopa.(none)>
2014-09-23 05:34:17 -0400

    wall follwer improved

--- warmup_project/scripts/wall_follower-2.py
+++ warmup_project/scripts/wall_follower-2.py
@@ -9,43 +9,52 @@ from math import *
 distance = 0.8
 dToWall = 1
 range90 = 0
+span = 20
 diff = 0.0
 lin = 0.1
 ang = 0.0
 
 def callback(data):
-    global dToWall, diff, range90
+    global dToWall, range90, range1, range2, diff
     dToWall = data.ranges[0]
     range90 = data.ranges[90]
-    diff = data.ranges[45] - data.ranges[135]
+    range1 = data.ranges[90 - span]
+    range2 = data.ranges[90 + span]
+    diff = range1 - range2
     print 'distance to wall =' + str(dToWall)
 
 def teleop(pub,lin,ang):
-    print'beforepub'
     pub.publish(Twist(linear = Vector3(x=lin),angular = Vector3(z=ang)))
-    print'afterpub'
 def main():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("scan", LaserScan, callback)
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    r = rospy.Rate(10) # 10hz
+    r = rospy.Rate(5) # 10hz
     while not rospy.is_shutdown():
-        print distance
         print dToWall
         while distance <= dToWall:
-            print 'in loop 1'
-            teleop(pub,0.08,ang)
+            teleop(pub,0.15,ang)
             r.sleep()
         teleop(pub,0,0)
-        while range90 == 0 or diff >= 0.01:
-            #print 'diff = ' + str(diff)
-            print 'in loop 2'
-            teleop(pub,0,-0.2)
-            r.sleep()
         while dToWall >= 0.3 or dToWall == 0:
-            print 'in loop 3'
-            teleop(pub,1.0,0)
-            r.sleep()
+            print 'range90 = ' + str(range90)
+            print 'range1 = ' + str(range1)
+            print 'range2 = ' + str(range2)
+            print 'diff = ' + str(diff)
+            if range90 == 0 or range1 == 0 or range2 == 0:
+                teleop(pub,0,-0.5)
+                r.sleep()
+            elif diff >= 0.05:
+                teleop(pub,0,0.5)
+                r.sleep()
+            elif diff <= -0.05:
+                teleop(pub,0,-0.5)
+                r.sleep()
+                print 'spinning =D'
+            else:
+                teleop(pub,0.15,0)
+                print 'running forward'
+                r.sleep()
   
 if __name__ == '__main__':
     try:
